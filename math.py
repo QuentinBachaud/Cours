4cb5789fd62a91f04039517da3019cbadb3eb8ae
QuentinBachaud <96500842+QuentinBachaud@users.noreply.github.com>
2026-05-15 21:59:18 +0200

    Ajouter la fonction sub() pour la soustraction

--- math.py
+++ math.py
@@ -10,3 +10,17 @@ def add(a, b):
         La somme de a et b
     """
     return a + b
+
+
+def sub(a, b):
+    """
+    Soustrait deux nombres et retourne le résultat.
+    
+    Args:
+        a: Le premier nombre
+        b: Le deuxième nombre
+    
+    Returns:
+        La différence entre a et b
+    """
+    return a - b
