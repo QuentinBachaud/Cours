a2942eda019270525c2f79f79acbeee3f643f4a3
QuentinBachaud <96500842+QuentinBachaud@users.noreply.github.com>
2026-05-15 21:51:04 +0200

    Ajouter la fonction add() au fichier math.py

---
+++ math.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    """
+    Additionne deux nombres et retourne le résultat.
+    
+    Args:
+        a: Le premier nombre
+        b: Le deuxième nombre
+    
+    Returns:
+        La somme de a et b
+    """
+    return a + b
